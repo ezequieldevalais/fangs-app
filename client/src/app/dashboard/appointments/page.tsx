7510ad4fd8a68465acab4556045ab84d9e5ad1bb
Ezequiel de Valais <ezequiel.devalais@gmail.com>
2024-04-10 12:23:50 -0300

    test devexpress scheduler

--- client/src/app/dashboard/appointments/page.tsx
+++ client/src/app/dashboard/appointments/page.tsx
@@ -1,15 +1,34 @@
 import * as React from 'react';
-import type { Metadata } from 'next';
-import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
-import { config } from '@/config';
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
 
-export const metadata = { title: `Users | Dashboard | ${config.site.name}` } satisfies Metadata;
 
 export default function Page(): React.JSX.Element {
   return (
-    <Stack spacing={3}>
-      hi
-    </Stack>
+    <Paper>
+      <Scheduler
+        data={schedulerData}
+      >
+        <ViewState
+          currentDate={currentDate}
+        />
+        <DayView
+          startDayHour={9}
+          endDayHour={14}
+        />
+        <Appointments />
+      </Scheduler>
+    </Paper>
   );
 }
